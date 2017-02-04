deb5e95cfb7e4262c8ea3c20dc624b02fe29522d
sfabini <scott.fabini@gmail.com>
2017-02-03 16:07:21 -0800

    performance optimization - get output during training. add command-line parsing.

--- 2_multi-layer/Main.py
+++ 2_multi-layer/Main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from Network import Network
 import sys
 from ParseFile import Parser
@@ -5,6 +7,7 @@ from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
 import getopt
 import numpy as np
+import random
 np.set_printoptions(threshold=np.nan)
 
 
@@ -12,43 +15,75 @@ class Main:
 
     def main(self, argv):
 
-        # process command line arguments
-        try:
-            opts, args = getopt.getopt(argv, "n:m:l:h", ["max-train=", "max-test="])
-        except getopt.GetoptError:
-            print('Main.py -n <hidden layer size> -o <output layer size> -m <momentum> -l <learning rate> -e <epochs>')
-            sys.exit(2)
-        hidden_layer_size = 50
+        # initialize command line parameter defaults
+        hidden_layer_size = 20
         output_layer_size = 10
         momentum = 0.9
         learning_rate = 0.1
         epochs = 50
-        max_train_inputs = 600
-        max_test_inputs = 100
+        max_train_inputs = 60000
+        max_test_inputs = 10000
+        accuracy_file = "accuracy.txt"
+        confusion_file = "confusion.txt"
+        randomize = False
+
+        # process command line arguments
+        try:
+            opts, args = getopt.getopt(argv[1:], "n:m:l:h", ["max-train=", "max-test=", "accuracy-file=", "confusion-file="])
+        except getopt.GetoptError:
+            print('Main.py -n <hidden layer size> -o <output layer size> -m <momentum> -l <learning rate> -e <epochs>')
+            sys.exit(2)
         for opt, arg in opts:
             if opt == '-h':
                 print('Main.py -n <hidden layer size> -o <output layer size> '
                       '-m <momentum> -l <learning_rate> -e <epochs>')
                 sys.exit()
-            elif opt in "-n":
-                hidden_layer_size = arg
-            elif opt in "-o":
-                output_layer_size = arg
-            elif opt in "-m":
-                momentum = arg
-            elif opt in "-l":
-                learning_rate = arg
-            elif opt in "-e":
-                epochs = arg
-            elif opt in "--max-train":
-                max_train_inputs = arg
-            elif opt in "--max-test":
-                max_test_inputs = arg
+            elif opt == "-n":
+                hidden_layer_size = int(arg)
+            elif opt == "-o":
+                output_layer_size = int(arg)
+            elif opt == "-m":
+                momentum = float(arg)
+            elif opt == "-l":
+                learning_rate = float(arg)
+            elif opt == "-e":
+                epochs = int(arg)
+            elif opt == "--max-train":
+                randomize = True
+                max_train_inputs = int(arg)
+            elif opt == "--max-test":
+                max_test_inputs = int(arg)
+            elif opt == "--accuracy-file":
+                accuracy_file = './results/' + arg
+            elif opt == "--confusion-file":
+                confusion_file = './results/' + arg
+
+        print('Configuration: \n'
+              'Output Layers: {0}\n' 'Hidden Layers: {1}\n'
+              'Epochs: {2}\n' 'Learning Rate: {3}\n'
+              'Momentum: {4}\n'
+              'Max Training Images: {5}\n' 'Max Test Images: {6}\n'
+              'Accuracy File: {7}\n' 'Confusion File: {8}\n'
+              .format(output_layer_size, hidden_layer_size, epochs, learning_rate, momentum,
+                      max_train_inputs, max_test_inputs, accuracy_file, confusion_file))
+
 
         # parse input files
         parser = Parser()
         print('Begin Parsing Training Set...')
-        training_images, training_targets = parser.parse_file('mnist_train.csv', max_train_inputs)
+        training_images, training_targets = parser.parse_file('mnist_train.csv', 60000)
+
+        # optionally pick randomized subset of training images
+        if randomize:
+            random_indexes = random.sample(range(60000), max_train_inputs)
+            randomized_training_images = []
+            randomized_training_targets = []
+            for i in random_indexes:
+                randomized_training_images.append(training_images[i])
+                randomized_training_targets.append(training_targets[i])
+            training_images = randomized_training_images
+            training_targets = randomized_training_targets
+
         image_size = len(training_images[0])
         number_of_training_images = len(training_images)
         print('Parsing Training Set Complete.\n')
@@ -57,12 +92,6 @@ class Main:
         test_images, test_targets = parser.parse_file('mnist_test.csv', max_test_inputs)
         number_of_test_images = len(test_images)
         print('Parsing Test Set Complete.\n')
-
-        # initialize output files as blank
-        f = open('accuracies.csv', 'w+')
-        f.close()
-        f = open('confusion.txt', 'w+')
-        f.close()
 
         # initialize network and per-epoch output variables
         network = Network(image_size, hidden_layer_size, output_layer_size, learning_rate, momentum)
@@ -76,20 +105,24 @@ class Main:
         test_prediction_vectors = [0] * number_of_test_images
         test_accuracies = []
 
+        # epoch 0 training setup
+        training_prediction_vectors[0] = network.get_output(training_images[0])
+        training_predictions[0] = self.vector_to_numeric(training_prediction_vectors[0])
+
+
         # begin learning
         print('Begin Learning With Learning Rate: {0}'.format(learning_rate))
         for epoch in range(epochs):
-            for i in range(number_of_training_images):
-                training_prediction_vectors[i] = network.get_output(training_images[i])
-                training_predictions[i] = self.vector_to_numeric(training_prediction_vectors[i])
-            previous_epoch_training_accuracy = training_accuracy
-            training_accuracy = accuracy_score(training_targets, training_predictions) * 100
-            training_accuracies.append(training_accuracy)
 
             # get test predictions
             for i in range(number_of_test_images):
                 test_prediction_vectors[i] = network.get_output(np.array(test_images[i]))
                 test_predictions[i] = self.vector_to_numeric(test_prediction_vectors[i])
+
+            # accumulate results for this epoch
+            previous_epoch_training_accuracy = training_accuracy
+            training_accuracy = accuracy_score(training_targets, training_predictions) * 100
+            training_accuracies.append(training_accuracy)
             test_accuracy = accuracy_score(test_targets, test_predictions) * 100
             test_accuracies.append(test_accuracy)
 
@@ -101,18 +134,12 @@ class Main:
 
             # train based on predictions vs. targets
             for i in range(number_of_training_images):
-                # print('prediction: {0}'.format(training_predictions[i]))
-                # print('target: {0}'.format(training_targets[i]))
-                #if training_predictions[i] != training_targets[i]:
-                    training_target_vector = self.numeric_to_vector(training_targets[i])
-
-                    network.train(training_images[i], training_target_vector)
-
-            #network.reset_for_epoch()
+                training_target_vector = self.numeric_to_vector(training_targets[i])
+                training_prediction_vectors[i] = network.train(training_images[i], training_target_vector)
+                training_predictions[i] = self.vector_to_numeric(training_prediction_vectors[i])
 
             # break out of epoch loop if training accuracy > 80% and delta is small
-            # if abs(previous_epoch_training_accuracy - training_accuracy) <= 1.0:
-            if training_accuracy > 70 and abs(previous_epoch_training_accuracy - training_accuracy) <= 1.0:
+            if training_accuracy > 70 and abs(previous_epoch_training_accuracy - training_accuracy) <= 0.01:
                 break
 
         # display final results
@@ -126,9 +153,7 @@ class Main:
         print(str(confusion_matrix(test_targets, test_predictions)) + '\n\n')
 
         # write accuracy results to files
-        f = open('accuracies.csv'.format(learning_rate), 'a')
-        f.write('Learning Rate: {0}\n'.format(learning_rate))
-        f.write('Training Accuracies:\n')
+        f = open(accuracy_file, 'w+')
         f.write(str([value for value in range(len(training_accuracies))]).strip('[]') + '\n')
         f.write(str([value for value in training_accuracies]).strip('[]') + '\n\n')
         f.write('Test Accuracies:\n')
@@ -137,7 +162,7 @@ class Main:
         f.close()
 
         # write confusion matrix results to files
-        f = open('confusion.txt', 'a')
+        f = open(confusion_file, 'w+')
         f.write('Learning Rate: {0}\n'.format(learning_rate))
         f.write('Training Confusion Matrix: \n')
         f.write(str(confusion_matrix(training_targets, training_predictions)) + '\n\n')
@@ -147,6 +172,7 @@ class Main:
 
         return None
 
+    # helper functions to convert from numeric value (e.g. 3) to array (.1,.1,.1,.9,.1,.1,.1,.1,.1) and back
     def vector_to_numeric(self, vector):
         return np.argmax(vector)
 
--- 2_multi-layer/ParseFile.py
+++ 2_multi-layer/ParseFile.py
@@ -1,14 +1,19 @@
+#!/usr/bin/env python3
+
 import numpy as np
+import sys
+import getopt
+
 
 class Parser:
     def parse_file(self, input_filename, max_inputs_to_parse):
-        f = open(input_filename, 'r')
+        file = open(input_filename, 'r')
         outputs = []
         inputs = []
         bias_input = 1.0
-        for i, line in enumerate(f):
-            if i > max_inputs_to_parse:
-                break #for debug, limits number of images to process
+        for i, line in enumerate(file):
+            if (i > max_inputs_to_parse):
+                break  # for debug, limits number of images to process
             line = line.strip().split(',')
             line = [int(x) for x in line]
             outputs.append(line[0])
@@ -17,8 +22,79 @@ class Parser:
             inputs[i].insert(0, bias_input)
         return np.array(inputs), np.array(outputs)
 
+'''
+    Incomplete. Return to later.
 
+    def read_preparsed_files(self):
+        images_file = open('images.csv', 'r')
+        targets_file = open('targets.csv', 'r')
+        images = []
+        targets = []
+        bias_input = 1.0
+        for i, line in enumerate(images_file):
+            line = line.strip().strip('[]').split(',')
+            print(line)
+            line = [int(x) for x in line]
+            images.append(np.array(line[1:]))
+            images[i] = list([x/255 for x in images[i]])
+            images[i].insert(0, bias_input)
+        for i, line in enumerate(targets_file):
+            line = line.strip().split(',')
+            line = [int(x) for x in line]
+            targets.append(line[0])
+        print(images)
+        print(targets)
+        return np.array(images), np.array(targets)
+'''
+if __name__ == "__main__":
 
+    randomize = False
+    count = 10000
 
+    ifile = '/dev/null'
+    ofile_images = '/dev/null'
+    ofile_targets = '/dev/null'
 
+    # process command line arguments
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "i:h", ["o_images=", "o_targets=", "count="])
+    except getopt.GetoptError:
+        print('ParseFile.py -i <input_filename> -c <count_to_read> '
+              '--o_images=<output_filename> --o_targets=<output_filename> ')
+        sys.exit(2)
 
+    for opt, arg in opts:
+        if opt == '-h':
+            print('ParseFile.py -i <input_filename> -c <count_to_read> '
+                  '--o_images=<output_filename> --o_targets=<output_filename> ')
+            sys.exit()
+        elif opt == "-i":
+            ifile = arg
+        elif opt == "--o_images":
+            ofile_images = arg
+        elif opt == "--o_targets":
+            ofile_targets = arg
+        elif opt == "--count":
+            randomize = True
+            count = int(arg)
+
+    print('Configuration: \n'
+          'Input File: {0}\n' 'Output Images: {1}\n' 'Output Targets: {2}\n' 'Count: {3}'
+          .format(ifile, ofile_images, ofile_targets, count))
+
+    # parse input files
+    parser = Parser()
+
+    print('Begin Parsing Training Set...')
+    training_images, training_targets = parser.parse_file(ifile, count)
+
+    f = open(ofile_images, 'w+')
+    f.write(str([value for value in training_images.tolist()]) + '\n')
+    f.close()
+
+    f = open(ofile_targets, 'w+')
+    f.write(str([value for value in training_targets]) + '\n')
+    f.close()
+
+    print('Parsing File Complete.\n')
+    sys.exit(0)
