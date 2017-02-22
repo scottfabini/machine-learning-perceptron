53d21a1465fd4019003c8702447d3f3ea16fcbe7
sfabini <scott.fabini@gmail.com>
2017-02-22 10:57:26 -0800

    algorithm improvements. output confusion matrix and accuracy/recall.

--- 4_bayes/Main.py
+++ 4_bayes/Main.py
@@ -39,54 +39,85 @@ class Main:
         data = pd.read_csv('spambase/spambase.data', header=None, index_col=57)
         data = sklearn.utils.shuffle(data)
         X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(data, data.index.values, test_size=0.5)
+       
         
-        
+
         probability_class_is_zero = numpy.count_nonzero(X_train.index) / len(X_train.index)
         probability_class_is_one = (len(X_train.index) - numpy.count_nonzero(X_train.index)) / len(X_train.index)
         print("P(0): {0}, P(1): {1}".format(probability_class_is_zero * 100, probability_class_is_one * 100))
 
-        X_train_classified_as_zero = numpy.transpose(X_train.iloc[numpy.where(X_train.index == 0)[0]])
-        X_train_classified_as_one = numpy.transpose(X_train.iloc[numpy.where(X_train.index == 1)[0]])
-  
-        #marginal_probability_of_data 
-                
-
-        X_means_given_zero = numpy.mean(X_train_classified_as_zero, axis=1)
-        X_means_given_one = numpy.mean(X_train_classified_as_one, axis=1)
-        X_std_given_zero = numpy.std(X_train_classified_as_zero, axis=1) 
-        X_std_given_one = numpy.std(X_train_classified_as_one, axis=1) 
-        print("\n\nmean(X|1):\n{0}, \nmean(X|0):\n{1}".format(X_means_given_one, X_means_given_one))
-        print("\n\nstd(X|1):\n{0}, \nstd(X|0):\n{1}".format(X_std_given_one, X_std_given_one))
-        print("\n\nX classified 0:\n{0}".format(X_train_classified_as_zero))
-        print("\n\nX classified 1:\n{0}".format(X_train_classified_as_one))
-        
-        '''
-        print("\n\nX classified 0:\n{0}".format(numpy.shape(numpy.transpose(X_train_classified_as_zero))))
-        print("\n\nX classified 1:\n{0}".format(numpy.shape(numpy.transpose(X_train_classified_as_one))))
-        print("\n\nmean(X|1):\n{0}, \nmean(X|0):\n{1}".format(numpy.shape(X_means_given_one), numpy.shape(X_means_given_one)))
-        print("\n\nstd(X|1):\n{0}, \nstd(X|0):\n{1}".format(numpy.shape(X_std_given_one), numpy.shape(X_std_given_one)))
-
-        print("\n\nzip(X|1):\n{0}".format(list(zip(X_means_given_one, X_std_given_one, numpy.transpose(X_train_classified_as_one)))))
-        print("\n\nzip(X|0):\n{0}".format(list(zip(X_means_given_zero, X_std_given_zero, numpy.transpose(X_train_classified_as_zero)))))
-        print("\n\nzip(X|1):\n{0}".format(numpy.shape(list(zip(X_means_given_one, X_std_given_one, numpy.transpose(X_train_classified_as_one))))))
-        print("\n\nzip(X|0):\n{0}".format(numpy.shape(list(zip(X_means_given_zero, X_std_given_zero, numpy.transpose(X_train_classified_as_zero))))))
-        '''
-        probability_x_given_class_is_zero =  [1 / (std * math.sqrt(2 * math.pi)) * math.exp(-(x - mean)**2 / (2 * std **2)) \
-            for mean, std, x in zip(X_means_given_zero, X_std_given_zero, X_train_classified_as_zero) if mean != 0]
-        probability_x_given_class_is_one =  [1 / (std * math.sqrt(2 * math.pi)) * math.exp(-(x - mean)**2 / (2 * std **2)) \
-            for mean, std, x in zip(X_means_given_one, X_std_given_one, X_train_classified_as_one) if mean != 0]
+        X_train_classified_as_zero = X_train.iloc[numpy.where(X_train.index == 0)[0]]
+        X_train_classified_as_one = X_train.iloc[numpy.where(X_train.index == 1)[0]]
          
-        probability_of_feature = [numpy.sum(x * probability_class_is_one) for x in probability_x_given_class_is_one]
-        print("P(x|c=0): {0}\n\n Shape: {1}\n\n".format(probability_x_given_class_is_zero, numpy.shape(probability_x_given_class_is_zero)))
-        print("P(x|c=1): {0}\n\n, Shape: {1}\n\n".format(probability_x_given_class_is_one, numpy.shape(probability_x_given_class_is_one)))
-        print("Probability of feature: {0}".format(probability_of_feature))
+        feature_means_given_zero = X_train_classified_as_zero.mean(axis=0)
+        feature_means_given_one = X_train_classified_as_one.mean(axis=0)
+        feature_std_given_zero = X_train_classified_as_zero.std(axis=0) 
+        feature_std_given_one = X_train_classified_as_one.std(axis=0) 
         
-        class_of_x_given_zero = math.log(probability_class_is_zero) + numpy.sum([math.log(xi_given_class) for xi_given_class in probability_x_given_class_is_zero if xi_given_class != 0])
-        class_of_x_given_one = math.log(probability_class_is_one) + numpy.sum([math.log(xi_given_class) for xi_given_class in probability_x_given_class_is_one if xi_given_class != 0])
+        print("\n\n\n\n")
+        print(numpy.shape(feature_means_given_one))
+        print(numpy.shape(feature_means_given_zero))
+        
+        print(numpy.shape(feature_std_given_one))
+        print(numpy.shape(feature_std_given_zero))        
+
+        probability_x_given_class_is_zero =  [[1 / (std * math.sqrt(2 * math.pi)) * math.exp(-(x - mean)**2 / (2 * std **2)) \
+            for mean, std, x in zip(feature_means_given_zero, feature_std_given_zero, x_rows) if mean != 0] for x_rows in X_train.values]
+        probability_x_given_class_is_one =  [[1 / (std * math.sqrt(2 * math.pi)) * math.exp(-(x - mean)**2 / (2 * std **2)) \
+            for mean, std, x in zip(feature_means_given_one, feature_std_given_one, x_rows) if mean != 0] for x_rows in X_train.values]
+         
+        #print("P(x|c=0): {0}\n\n Shape: {1}\n\n".format(probability_x_given_class_is_zero, numpy.shape(probability_x_given_class_is_zero)))
+        #print("P(x|c=1): {0}\n\n, Shape: {1}\n\n".format(probability_x_given_class_is_one, numpy.shape(probability_x_given_class_is_one)))
+        
+        #print("P(x|c=0): Shape: {0}\n\n".format(numpy.shape(probability_x_given_class_is_zero)))
+        #print("P(x|c=1): Shape: {0}\n\n".format(numpy.shape(probability_x_given_class_is_one)))
+        
+        '''
+        probability_of_feature_zero = [numpy.sum(probability_class_is_one) for x in probability_x_given_class_is_one]
+        probability_of_feature_one = [numpy.sum(probability_class_is_one) for x in probability_x_given_class_is_zero]
+        classification_of_feature_zero = numpy.argmax(probability_of_feature_zero)
+        classification_of_feature_one = numpy.argmax(probability_of_feature_one)
+        print("Probability of feature: {0}".format(probability_of_feature_one))
+        print("Classification of feature: {0}".format(classification_of_feature_zero))
+        print("Probability of feature: {0}".format(probability_of_feature_one))
+        print("Classification of feature: {0}".format(classification_of_feature_one))
+
+
+        '''
+        
+        #print([[math.log(xi) for xi in x_rows if xi != 0] for x_rows in probability_x_given_class_is_zero])
+        print(probability_class_is_zero)
+        print(probability_class_is_one)
+        class_of_x_given_zero = math.log(probability_class_is_zero) + numpy.array([numpy.sum([math.log(xi) for xi in x_rows if xi != 0]) for x_rows in probability_x_given_class_is_zero])
+        class_of_x_given_one = math.log(probability_class_is_one) + numpy.array([numpy.sum([math.log(xi) for xi in x_rows if xi != 0]) for x_rows in probability_x_given_class_is_one])
+        
+
+        #class_of_x_given_one = [numpy.sum([math.log(xi) for xi in x_rows if xi != 0]) for x_rows in probability_x_given_class_is_zero]
+        
+        #print(class_of_x_given_zero)
+
+        #class_of_x_given_zero = math.log(probability_class_is_zero) + numpy.sum([math.log(xi_given_class) for xi_given_class in probability_x_given_class_is_zero if xi_given_class != 0])
+        #class_of_x_given_one = math.log(probability_class_is_one) + numpy.sum([math.log(xi_given_class) for xi_given_class in probability_x_given_class_is_one if xi_given_class != 0])
         
         print("class | 0: {0}".format(class_of_x_given_zero))
-
         print("class | 1: {0}".format(class_of_x_given_one))
+
+        class_prediction = [1 if a > b else 0 for a, b in zip(class_of_x_given_one, class_of_x_given_zero)]
+
+        print(sklearn.metrics.classification_report(class_prediction, y_train))
+        print(sklearn.metrics.confusion_matrix(class_prediction, y_train))
+        ''' 
+        print([X_train_row for X_train_row in X_train])
+
+        print(list(X_train.iloc[0]))
+        print(list(X_train.iloc[1]))
+        print(list(X_train.iloc[2]))
+
+        test = [[1 / (std * math.sqrt(2 * math.pi)) * math.exp(-(xi - mean)**2 / (2 * std **2)) \
+            for mean, std, xi in zip(X_means_given_one, X_std_given_one, X_train_row)] for X_train_row in X_train]
+        
+        #print([xi_given_class for xi_given_class in probability_x_given_class_is_zero if xi_given_class != 0])
+        '''
         '''
         [ numpy.argmax( math.log())for i in range(57)]
         # Normalize the data by scaling to the training data
