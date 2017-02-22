101048d23318b00f0811ec74dcfbe1de964fe9f4
sfabini <scott.fabini@gmail.com>
2017-02-22 10:58:12 -0800

    algorithm improvements. output confusion matrix and accuracy/recall.

--- 4_bayes/Main.py
+++ 4_bayes/Main.py
@@ -53,52 +53,17 @@ class Main:
         feature_means_given_one = X_train_classified_as_one.mean(axis=0)
         feature_std_given_zero = X_train_classified_as_zero.std(axis=0) 
         feature_std_given_one = X_train_classified_as_one.std(axis=0) 
-        
-        print("\n\n\n\n")
-        print(numpy.shape(feature_means_given_one))
-        print(numpy.shape(feature_means_given_zero))
-        
-        print(numpy.shape(feature_std_given_one))
-        print(numpy.shape(feature_std_given_zero))        
 
         probability_x_given_class_is_zero =  [[1 / (std * math.sqrt(2 * math.pi)) * math.exp(-(x - mean)**2 / (2 * std **2)) \
             for mean, std, x in zip(feature_means_given_zero, feature_std_given_zero, x_rows) if mean != 0] for x_rows in X_train.values]
         probability_x_given_class_is_one =  [[1 / (std * math.sqrt(2 * math.pi)) * math.exp(-(x - mean)**2 / (2 * std **2)) \
             for mean, std, x in zip(feature_means_given_one, feature_std_given_one, x_rows) if mean != 0] for x_rows in X_train.values]
          
-        #print("P(x|c=0): {0}\n\n Shape: {1}\n\n".format(probability_x_given_class_is_zero, numpy.shape(probability_x_given_class_is_zero)))
-        #print("P(x|c=1): {0}\n\n, Shape: {1}\n\n".format(probability_x_given_class_is_one, numpy.shape(probability_x_given_class_is_one)))
         
-        #print("P(x|c=0): Shape: {0}\n\n".format(numpy.shape(probability_x_given_class_is_zero)))
-        #print("P(x|c=1): Shape: {0}\n\n".format(numpy.shape(probability_x_given_class_is_one)))
-        
-        '''
-        probability_of_feature_zero = [numpy.sum(probability_class_is_one) for x in probability_x_given_class_is_one]
-        probability_of_feature_one = [numpy.sum(probability_class_is_one) for x in probability_x_given_class_is_zero]
-        classification_of_feature_zero = numpy.argmax(probability_of_feature_zero)
-        classification_of_feature_one = numpy.argmax(probability_of_feature_one)
-        print("Probability of feature: {0}".format(probability_of_feature_one))
-        print("Classification of feature: {0}".format(classification_of_feature_zero))
-        print("Probability of feature: {0}".format(probability_of_feature_one))
-        print("Classification of feature: {0}".format(classification_of_feature_one))
-
-
-        '''
-        
-        #print([[math.log(xi) for xi in x_rows if xi != 0] for x_rows in probability_x_given_class_is_zero])
-        print(probability_class_is_zero)
-        print(probability_class_is_one)
         class_of_x_given_zero = math.log(probability_class_is_zero) + numpy.array([numpy.sum([math.log(xi) for xi in x_rows if xi != 0]) for x_rows in probability_x_given_class_is_zero])
         class_of_x_given_one = math.log(probability_class_is_one) + numpy.array([numpy.sum([math.log(xi) for xi in x_rows if xi != 0]) for x_rows in probability_x_given_class_is_one])
         
 
-        #class_of_x_given_one = [numpy.sum([math.log(xi) for xi in x_rows if xi != 0]) for x_rows in probability_x_given_class_is_zero]
-        
-        #print(class_of_x_given_zero)
-
-        #class_of_x_given_zero = math.log(probability_class_is_zero) + numpy.sum([math.log(xi_given_class) for xi_given_class in probability_x_given_class_is_zero if xi_given_class != 0])
-        #class_of_x_given_one = math.log(probability_class_is_one) + numpy.sum([math.log(xi_given_class) for xi_given_class in probability_x_given_class_is_one if xi_given_class != 0])
-        
         print("class | 0: {0}".format(class_of_x_given_zero))
         print("class | 1: {0}".format(class_of_x_given_one))
 
