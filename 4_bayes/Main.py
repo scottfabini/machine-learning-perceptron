03525a3032857c7428b72b194e8fe2fc9fce0890
sfabini <scott.fabini@gmail.com>
2017-02-22 18:03:34 -0800

    unroll list comprehensions into numpy operations.

--- 4_bayes/Main.py
+++ 4_bayes/Main.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
 """
-Homework 3 for CS545 Machine Learning at Portland State University
+Homework 4 for CS545 Machine Learning at Portland State University
 
-Perform experiments on Support Vector Machines (SVM), using HP's Spambase data.
+Perform experiments on Gaussian Bayes Classifiers, using HP's Spambase data.
 https://archive.ics.uci.edu/ml/datasets/Spambase
 
 """
@@ -20,6 +20,7 @@ import sklearn.svm
 import random
 import math
 import sys
+#numpy.set_printoptions(threshold=numpy.nan)
 
 
 class Main:
@@ -28,8 +29,8 @@ class Main:
         """
         Experiment 1:
 
-        Split the data in 1/2, into training data and test data. Train the SVM using the training data.
-        Then run the trained SVM on the test data. Report out on accuracy, precision, recall, and ROC curve.
+        Split the data in into training data and test data. Train the Naive Gaussian Bayes classifier using the training data.
+        Then run the trained classifier on the test data. Report out on accuracy, precision, recall.
 
         """
 
@@ -39,39 +40,98 @@ class Main:
         data = pd.read_csv('spambase/spambase.data', header=None, index_col=57)
         data = sklearn.utils.shuffle(data)
         X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(data, data.index.values, test_size=0.5)
-       
-        
 
-        probability_class_is_zero = numpy.count_nonzero(X_train.index) / len(X_train.index)
-        probability_class_is_one = (len(X_train.index) - numpy.count_nonzero(X_train.index)) / len(X_train.index)
+        # Determine class probabilities P(0) and P(1)
+        probability_class_is_one = numpy.count_nonzero(X_train.index) / len(X_train.index)
+        probability_class_is_zero = (len(X_train.index) - numpy.count_nonzero(X_train.index)) / len(X_train.index)
         print("P(0): {0}, P(1): {1}".format(probability_class_is_zero * 100, probability_class_is_one * 100))
 
+        
+        # Split the data into those training data in class 1 and those in class 0
         X_train_classified_as_zero = X_train.iloc[numpy.where(X_train.index == 0)[0]]
         X_train_classified_as_one = X_train.iloc[numpy.where(X_train.index == 1)[0]]
          
+        # Determine the means and the standard deviations of the training data in class 1 and class 0
         feature_means_given_zero = X_train_classified_as_zero.mean(axis=0)
-        feature_means_given_one = X_train_classified_as_one.mean(axis=0)
         feature_std_given_zero = X_train_classified_as_zero.std(axis=0) 
+        feature_means_given_one = X_train_classified_as_one.mean(axis=0)
         feature_std_given_one = X_train_classified_as_one.std(axis=0) 
 
-        probability_x_given_class_is_zero =  [[1 / (std * math.sqrt(2 * math.pi)) * math.exp(-(x - mean)**2 / (2 * std **2)) \
-            for mean, std, x in zip(feature_means_given_zero, feature_std_given_zero, x_rows) if mean != 0] for x_rows in X_train.values]
-        probability_x_given_class_is_one =  [[1 / (std * math.sqrt(2 * math.pi)) * math.exp(-(x - mean)**2 / (2 * std **2)) \
-            for mean, std, x in zip(feature_means_given_one, feature_std_given_one, x_rows) if mean != 0] for x_rows in X_train.values]
-         
+        # Deterimine P(xi | 0) and P(xi | 1) using Naive Bayes Gaussian formula
+        probability_x_given_class_is_zero = 1 / (feature_std_given_zero * numpy.sqrt(2 * math.pi)) \
+                                                * numpy.exp(-(X_train.iloc[0] - feature_means_given_zero)**2 / (2 * feature_std_given_zero **2))
+
+        probability_x_given_class_is_one = 1 / (feature_std_given_one * numpy.sqrt(2 * math.pi)) \
+                                               * numpy.exp(-(X_train.iloc[0] - feature_means_given_one)**2 / (2 * feature_std_given_one **2))
+        for i in range(2300):    
+            probability_xi_given_class_is_zero = 1 / (feature_std_given_zero * numpy.sqrt(2 * math.pi)) \
+                                                * numpy.exp(-(X_train.iloc[i] - feature_means_given_zero)**2 / (2 * feature_std_given_zero **2))
+
+            probability_xi_given_class_is_one = 1 / (feature_std_given_one * numpy.sqrt(2 * math.pi)) \
+                                               * numpy.exp(-(X_train.iloc[i] - feature_means_given_one)**2 / (2 * feature_std_given_one **2))
+            
+            probability_x_given_class_is_zero = numpy.column_stack((probability_x_given_class_is_zero, probability_xi_given_class_is_zero))
+            probability_x_given_class_is_one = numpy.column_stack((probability_x_given_class_is_one, probability_xi_given_class_is_one))
         
-        class_of_x_given_zero = math.log(probability_class_is_zero) + numpy.array([numpy.sum([math.log(xi) for xi in x_rows if xi != 0]) for x_rows in probability_x_given_class_is_zero])
-        class_of_x_given_one = math.log(probability_class_is_one) + numpy.array([numpy.sum([math.log(xi) for xi in x_rows if xi != 0]) for x_rows in probability_x_given_class_is_one])
+        #return probability_x_given_class_is_zero, probability_x_given_class_is_one
+            
+        # Determine class(x) = argmax {  log(P(0)) + sum(logs(xi|0))  }
+        #                             {  log(P(1)) + sum(logs(xi|1))  }
+        probability_class_of_x_is_zero = numpy.zeros(2300)
+        probability_class_of_x_is_one = numpy.zeros(2300)
+        for i in range(2300):
+            probability_class_of_x_is_zero[i] = numpy.log(probability_class_is_zero) + \
+                                         numpy.sum(numpy.ma.log(probability_x_given_class_is_zero[:,i]))
+            probability_class_of_x_is_one[i] = numpy.log(probability_class_is_one) + \
+                                        numpy.sum(numpy.ma.log(probability_x_given_class_is_one[:,i])) 
+        
+        class_prediction_X_train = [1 if a > b else 0 for a, b in \
+                                   zip(probability_class_of_x_is_one, probability_class_of_x_is_zero)]
+
+        # Display results
+        
         
 
-        print("class | 0: {0}".format(class_of_x_given_zero))
-        print("class | 1: {0}".format(class_of_x_given_one))
+        #print("class | 0: {0}".format(numpy.shape(probability_class_of_x_is_zero)))
+        #print("class | 1: {0}".format(numpy.shape(probability_class_of_x_is_one)))
+        #print("class prediction: {0}".format(numpy.shape(class_prediction_X_train)))
+        #print("class prediction: {0}".format(numpy.shape(y_train)))
 
-        class_prediction = [1 if a > b else 0 for a, b in zip(class_of_x_given_one, class_of_x_given_zero)]
-
-        print(sklearn.metrics.classification_report(class_prediction, y_train))
-        print(sklearn.metrics.confusion_matrix(class_prediction, y_train))
+        print("Accuracy score: {0}".format(100 * sklearn.metrics.accuracy_score(class_prediction_X_train, y_train)))
+        print(sklearn.metrics.classification_report(class_prediction_X_train, y_train))
+        print(sklearn.metrics.confusion_matrix(class_prediction_X_train, y_train))
         
+        '''
+        print(numpy.shape(feature_means_given_one))
+        print(numpy.shape(feature_means_given_zero))
+        print(numpy.shape(X_train))
+        '''
+        '''
+        print("*******")
+        print(math.log(probability_class_is_zero))
+        print(probability_class_of_x_is_zero)
+        print("*******")
+        print(math.log(probability_class_is_one))
+        print(probability_class_of_x_is_one)
+        print("*******")
+        #print("\n\n\n\n")
+        #print(probability_x_given_class_is_zero)
+        #print("\n\n\n\n")  
+        #print(probability_x_given_class_is_one)
+        #print(X_train_classified_as_zero)
+        #print("*******")
+        #print(X_train_classified_as_one)
+        '''
+
+
+        '''
+        probability_class_of_x_is_zero = math.log(probability_class_is_zero) + \
+            numpy.array([numpy.sum([numpy.log(xi) for xi in x_rows if xi != 0]) 
+                                                 for x_rows in probability_x_given_class_is_zero])
+        probability_class_of_x_is_one = math.log(probability_class_is_one) + \
+            numpy.array([numpy.sum([numpy.log(xi) for xi in x_rows if xi != 0]) 
+                                                 for x_rows in probability_x_given_class_is_one])
+        '''
         return None
 
 if __name__ == "__main__":
